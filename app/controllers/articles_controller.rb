64fb8189363e5ea7084fc2df2b94470b7a6f706c
kareem <kareemhanysultan2004@gmail.com>
2026-06-12 16:07:06 +0300

    added the araticle controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,4 @@
-class ArticlesController < ApplicationController
+class Article < ApplicationRecord
   def index
     @articles = Article.all
   end
@@ -16,13 +16,12 @@ class ArticlesController < ApplicationController
     if @article.save
       redirect_to @article
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
-
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :content, :image)
   end
 end
