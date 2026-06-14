94fa375f3987e1230c4c7b656b755d14228ccd86
kareem <kareemhanysultan2004@gmail.com>
2026-06-14 14:45:22 +0300

    Add Many-to-Many Post-Editor association with join table

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
-  belongs_to :creator, class_name: 'User', foreign_key: 'creator_id
+  belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
+  
+  has_many :post_editors, dependent: :destroy
+  has_many :editors, through: :post_editors, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'creator_id', dependent: :destroy
+  
+  has_many :post_editors, dependent: :destroy
+  has_many :edited_posts, through: :post_editors, source: :post
 end
